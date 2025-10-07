71f2d33d13a6d47420141b7f06f9f6574c9d5465
Fabian Clarke <fclarke@wisc.edu>
2025-10-07 17:44:41 -0500

    Finished Basic SignIn and SignUp pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SignUp from './SignUp.jsx'
+import SignIn from './SignIn.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SignIn />
   </StrictMode>,
 )
