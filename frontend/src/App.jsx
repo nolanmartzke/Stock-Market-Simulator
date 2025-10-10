f4074e975aa6fdc707b3c0bd372096205fc5ee63
hmian2 <hmian2@wisc.edu>
2025-10-09 20:25:37 -0500

    fixed routes to be able to access signin/signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import AccountPage from "./pages/AccountPage";
+
 
 export default function App() {
   return (
-    <div className="app-root bg-light min-vh-100">
-      <NavBar />
-      <div className="main-content" style={{ marginLeft: "250px" }}>
-        <Home />
+    <>
+      <div className="app-root bg-light min-vh-100">
+        <NavBar />
+        <div className="main-content" style={{ marginLeft: "250px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/dashboard" element={<AccountPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
-import SignUp from "./SignUp.jsx";
-import SignIn from "./SignIn.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
