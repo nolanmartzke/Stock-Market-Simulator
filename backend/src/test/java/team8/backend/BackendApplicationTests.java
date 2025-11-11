299b17a638232221527280aa537038f2f46e85e3
rstrivedi2 <rstrivedi2@wisc.edu>
2025-11-11 06:43:43 -0600

    Corrected the failing tests. Now passing. Created new tests as well

--- backend/src/test/java/team8/backend/BackendApplicationTests.java
+++ backend/src/test/java/team8/backend/BackendApplicationTests.java
@@ -2,18 +2,10 @@ package team8.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-	properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-		"spring.jpa.hibernate.ddl-auto=create",
-		"spring.jpa.show-sql=false"
-	}
-)
+@SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
