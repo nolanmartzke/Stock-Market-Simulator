523fca0a6057bbe774890ee61b3a56ca6d424a54
hmian2 <hmian2@wisc.edu>
2025-12-02 18:23:18 -0600

    fixed cut off graph text

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -150,7 +150,7 @@ const Dashboard = () => {
                 </div>
                 <div className="flex-grow-1">
                   <ResponsiveContainer width="100%" height="100%">
-                    <AreaChart data={chartData} margin={{ top: 10, right: 10, left: -10, bottom: 0 }}>
+                    <AreaChart data={chartData} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
                       <defs>
                         <linearGradient id="areaGradient" x1="0" y1="0" x2="0" y2="1">
                           <stop offset="0%" stopColor="#60a5fa" stopOpacity={0.55} />
